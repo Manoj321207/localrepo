ece7625d1819f198480f24a10169fae237a4bc06
Manoj321207 <2022cs_manojjs_b@nie.ac.in>
2024-05-14 11:40:49 +0530

    update c program

--- first.c
+++ first.c
@@ -1,11 +1,12 @@
 #include<stdio.h>
 int main()
 {
-    int a=2,b=3,c=4,d=5,e=6;
+    int a=2,b=3,c=4,d=5,e=6,x=34981;
     int a3,a4;
     a3=a>b?(a>c?a:c):(b>c?b:c);
     printf("%d",a3);
     a4=a+b>c!=d==e;
-    printf("%d",a4);
+    printf("%d\n",a4);
+    printf("%d\n",x);
     return 0;
 }
